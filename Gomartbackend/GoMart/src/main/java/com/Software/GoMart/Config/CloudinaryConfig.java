f0e95c43d0ecd3aee4764b11c2f69770d4a7dfce
Avishkar709 <avishkarkumbhar777@gmail.com>
2024-08-26 14:58:58 +0530

    first commit

---
+++ Gomartbackend/GoMart/src/main/java/com/Software/GoMart/Config/CloudinaryConfig.java
@@ -0,0 +1,19 @@
+package com.Software.GoMart.Config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", "Cloud Name",
+            "api_key", "API KEY",
+            "api_secret", "API SECRETE"
+        ));
+    }
+}
